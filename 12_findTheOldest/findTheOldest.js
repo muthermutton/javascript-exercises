de26240d0cbc33f5af90b25aef0394f958e018ae
muthermutton <khairal.fazil@gmail.com>
2021-12-03 01:45:53 +0800

    complete excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let age1 = 0;
+    let age2 = 0;
+    let oldest = {};
 
+    arr.forEach((person, index) => {
+        if (!arr[index].yearOfDeath){
+            let year = new Date().getFullYear();
+            age1 = year - arr[index].yearOfBirth;
+        }else {
+            age1 = arr[index].yearOfDeath - arr[index].yearOfBirth;
+        };
+        if (age1 > age2) {
+            age2 = age1;
+            oldest = person;
+        };
+    })
+    return oldest;
 };
 
 // Do not edit below this line
